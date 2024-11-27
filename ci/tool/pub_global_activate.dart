62a8504cc533bfd91617fb00ebc13db6f951cd7e
Alexandre Roux <alex@tekartik.com>
2024-11-27 08:52:15 +0100

    setup

--- ci/tool/pub_global_activate.dart
+++
@@ -1,6 +0,0 @@
-import 'package:process_run/shell.dart';
-
-Future<void> main() async {
-  /// Run step
-  await run('dart pub global run tekartik_ci:setup_sqlite3lib');
-}
---
+++ ci/tool/pubglobalupdate_configure_dart3a_git.dart
@@ -0,0 +1,7 @@
+import 'package:process_run/shell.dart';
+
+/// To run after push.
+Future<void> main() async {
+  await run(
+      'dart pub global run pubglobalupdate --config-package tekartik_ci --source git --git-url https://github.com/tekartik/ci.dart --git-path ci --git-ref dart3a');
+}
